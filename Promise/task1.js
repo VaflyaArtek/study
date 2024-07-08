d74ffbe264c2189cda53136850f374cd734b6c91
VaflyaArtek <118203731+VaflyaArtek@users.noreply.github.com>
2024-07-08 23:33:09 +0300

    Update task1.js

--- Promise/task1.js
+++ Promise/task1.js
@@ -1,17 +1,17 @@
-import {urls, getPostsList, getUserInfo, getCommentsToPost} from './main.js';
+import { urls, fetchData } from './main.js';
 
-getPostsList(urls.postsList)
+fetchData(urls.postsList)
     .then(posts => {
         console.log('Post list: ', posts);
 
         const userId = prompt('Enter user id', '1');
-        return getUserInfo(urls.userInfo, userId);
+        return fetchData(urls.userInfo(userId));
     })
     .then(user => {
         console.log('User info: ', user);
 
         const postId = prompt('Enter post id', '1');
-        return getCommentsToPost(urls.commentsToPost, postId);
+        return fetchData(urls.commentsToPost(postId));
     })
-    .then(comment => console.log('Comment to post: ', comment))
+    .then(comments => console.log('Comment to post: ', comments))
     .catch(error => console.error('Error: ', error));
